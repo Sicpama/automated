41feb59e2b8454cc456917ec8c2ffe816f7e4f40
Chris Hewison <hewisonchris@gmail.com>
2023-08-27 17:30:10 +0900

    adding diner side order and pay

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import dotenv from 'dotenv';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,10 +24,12 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    // baseURL: 'https://store-dashboard-feature.sicpama.com',
+    isMobile: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    javaScriptEnabled: true,
+    viewport: { width: 412, height: 915 },
   },
 
   /* Configure projects for major browsers */
--- tests/login-store.spec.ts
+++ tests/login-store.spec.ts
@@ -1,17 +1,48 @@
-import { test, expect } from '@playwright/test';
+import path from 'path';
+import { test, expect, Page } from '@playwright/test';
 
-test('test login to store dashboard', async ({ page }) => {
-  await page.goto('https://store-dashboard-feature.sicpama.com/login/');
+import * as dotenv from 'dotenv';
+const env_path = path.join(process.cwd(), '../' , `.env${process.env.ENV === 'LOCAL' ? '.local' : ''}`);
+console.log(`env_path=${env_path}`);
+dotenv.config({path: env_path});
+const STORE_DASHBOARD_URL = process.env.STORE_DASHBOARD_URL || 'http://localhost:3000';
+const USR = process.env.USR || 'local2@sicpama.com';
+const PASS = process.env.PASS || 'testing1234';
+console.log(`STORE_DASHBOARD_URL=${STORE_DASHBOARD_URL}`);
+console.log(`USR=${USR}`);
+console.log(`PASS=${PASS}`);
+
+test.use({ viewport: { width: 1280, height: 720 } });
+
+test.describe('Store dashboard', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${STORE_DASHBOARD_URL}/login/`);
+    await expect(page).toHaveTitle(/SICPAMA Store Dashboard/);
+    await page.locator('input[name="email"]').click();
+    await page.locator('input[name="email"]').fill(USR);
+    await page.locator('input[name="password"]').click();
+    await page.locator('input[name="password"]').fill(PASS);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByText('Table status')).toBeVisible();
+    await expect(page.getByTestId('tableStatus_table-status-title')).toHaveText("Table status");
+  });
+
+  test.afterEach(async ({ page }) => {
+    page.close();
+  });
+
+  test('Check tabs', async ({ page }) => {
+    const tabs = page.getByRole('tab');
+    expect(await tabs.count()).toBe(3);
+    await expect(tabs.nth(0)).toHaveText('Table');
+    await expect(tabs.nth(1)).toHaveText('In progress');
+    await expect(tabs.nth(2)).toHaveText('Serving complete');
+  });
   
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/SICPAMA Store Dashboard/);
-
-  // Login
-  await page.locator('input[name="email"]').click();
-  await page.locator('input[name="email"]').fill('feature1@sicpama.com');
-  await page.locator('input[name="password"]').click();
-  await page.locator('input[name="password"]').fill('testing1234');
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  await expect(page.getByText('Table status')).toBeVisible();
+  test('clear table', async ({ page }) => {
+    await expect(page.getByTestId('TableList-table-card-11')).toBeVisible();
+    await page.getByTestId('TableList-table-card-11').click();
+    await page.getByTestId('OrderDetailModal_clear-table-button-div').click();
+    await page.getByRole('button', { name: 'OK' }).click();
+  });
 });
